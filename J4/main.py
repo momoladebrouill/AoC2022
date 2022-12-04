6fe3722d0d0dfe56201828969c14bdd1fb881763
momoladebrouill <momoladebrouill@gmail.com>
2022-12-04 06:52:10 +0100

    c'est plus compliqué en python qu'en C :wand:

--- J4/main.py
+++ J4/main.py
@@ -1,8 +1,13 @@
 f=open("input","r").read()
 f=f.split("\n")[:-1]
 
-s=0
+p1=0
+p2=0
 for ligne in f:
-    
+    (a1,a2),(b1,b2)=((int(n) for n in e.split('-')) for e in ligne.split(","))
+    p1+=(a1<=b1 and b2<=a2) or (b1<=a1 and a2<=b2) 
+    p2+=(a1<=b1 and b1<=a2) or (a1<=b2 and b2<=a2) or \
+        (b1<=a1 and a1<=b2) or (b1<=a2 and a2<=b2)
 
+print(p1,p2)
 
