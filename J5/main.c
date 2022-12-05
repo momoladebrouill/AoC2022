639cd74a59235f8398c546210fd7b6de82ffc68b
Ravelfett <ravelfett@gmail.com>
2022-12-05 19:33:02 +0100

    un pe avancé

--- J5/main.c
+++ J5/main.c
@@ -24,25 +24,31 @@ node* pop(node* s){
 
 int main(){
 
-  char stack[10][10];
+  char array[8][9];
   int l = 0;
 
-  FILE *fp = fopen("input","r");
+  FILE *fp = fopen("input.txt","r");
 
   while (l < 8) {
     char buf[BUF_SIZE];
     if (fgets(buf, BUF_SIZE, fp) == NULL) break; // no more line
     for(int j = 0; j < 9; j ++){
-      stack[l][j] = buf[j*4+1];
+      array[l][j] = buf[j*4+1];
     }
     l++;
   }
 
-
-  for(int i=0;i<7;i++){
-    for(int j=0;j<9;j++){
-      printf("%c",stack[i][j]);
+  for(int i = 0; i < 8; i++){
+    for(int j = 0; j < 9; j++){
+      printf("%c",array[i][j]);
     }
     printf("\n");
+  }
+
+  node* stack[9];
+  for(int i = 0; i < 9; i++){
+    for(int j = 8; j >= 0; j--){
+      stack[i] = push(stack[i], array[j][i]);
+    }
   }
 }
